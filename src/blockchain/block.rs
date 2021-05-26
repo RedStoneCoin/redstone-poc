3382f3e65e96ea1363cf672d93e064bd24a47626
toni <toni@pop-os.localdomain>
2021-05-26 11:44:42 +0200

    Added block lib

---
+++ src/blockchain/block.rs
@@ -0,0 +1,14 @@
+
+use std::fmt::{ self, Debug, Formatter };
+
+pub struct Block {
+}
+
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block")
+    }
+}
+
+impl Block {
+}
